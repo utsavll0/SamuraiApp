11b751b0f0578feaacaed7ecb3b2f93a748c9c39
UtsavkumarLal <utsavkumar.lal@us.brainscale.com>
2020-05-19 11:35:23 +0530

    Signed Commit

--- SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using SamuraiApp.Domain;
 using System;
 using System.Collections.Generic;
@@ -15,10 +16,19 @@ namespace SamuraiApp.Data
         public DbSet<Clan> Clans { get; set; }
         public DbSet<Battle> Battles { get; set; }
 
+        public static readonly ILoggerFactory ConsoleLoggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder
+                .AddFilter((category, level) => 
+                            category == DbLoggerCategory.Database.Command.Name 
+                            && level == LogLevel.Information)
+                .AddConsole();
+        });
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(
-                "Data source=(localdb)\\MSSQLLocalDB;Initial Catalog=SamuraiAppData");
+            optionsBuilder
+                .UseLoggerFactory(ConsoleLoggerFactory)
+                .UseSqlServer("Data source=(localdb)\\MSSQLLocalDB;Initial Catalog=SamuraiAppData");
         
         }
 
