247968093e2accadc15761d0dbf22711006ad089
UtsavkumarLal <utsavkumar.lal@us.brainscale.com>
2020-05-19 10:03:52 +0530

    First Commit

---
+++ SamuraiApp.Domain/Clan.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamuraiApp.Domain
+{
+    public class Clan
+    {
+        public int Id { get; set; }
+        public string ClanName { get; set; }
+    }
+}
